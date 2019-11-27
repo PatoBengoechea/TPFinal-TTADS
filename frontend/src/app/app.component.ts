bc782939a69d2fe2f7f5ebcc6c0bd98aabc031d5
Patricio Bengoechea <pato.bengoechea@gmail.com>
2019-11-27 18:29:49 -0300

    project with angular 8

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThrowStmt } from '../../node_modules/@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  
+  title = 'app';
+  option = 0;
+
+  constructor(){
+    const key="afbc1995a41f72f35622f748d82068dc";
+  }
+
+  setOption(opt: number){
+    this.option = opt;
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+// Service
+import { ApiThemoviedbService } from './api-themoviedb.service';
+
+// Routing
+import { AppRoutingModule } from './/app-routing.module'; 
+
+// Components
 import { AppComponent } from './app.component';
 import { BuscarpeliculasComponent } from './buscarpeliculas/buscarpeliculas.component';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { NowPlayingMoviesComponent } from './now-playing-movies/now-playing-movies.component';
 import { HomeComponent } from './home/home.component';
+import { PopularMoviesComponent } from './popular-movies/popular-movies.component';
 import { MovieComponent } from './movie/movie.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { NowPlayingMoviesComponent } from './now-playing-movies/now-playing-movies.component';
-import { PopularMoviesComponent } from './popular-movies/popular-movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BuscarpeliculasComponent,
+    NowPlayingMoviesComponent,
     HomeComponent,
+    PopularMoviesComponent,
     MovieComponent,
     MovieDetailsComponent,
-    NowPlayingMoviesComponent,
-    PopularMoviesComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApiThemoviedbService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
