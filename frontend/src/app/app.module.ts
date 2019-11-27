caaff6271b5cb71bc0421712dfcc1c22cbc9f0da
uoome <nicogomezwp@gmail.com>
2019-11-27 18:30:29 -0300

    Cambios menores

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BuscarpeliculasComponent } from './buscarpeliculas/buscarpeliculas.component';
-import { HomeComponent } from './home/home.component';
-import { MovieComponent } from './movie/movie.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { NowPlayingMoviesComponent } from './now-playing-movies/now-playing-movies.component';
-import { PopularMoviesComponent } from './popular-movies/popular-movies.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BuscarpeliculasComponent } from "./buscarpeliculas/buscarpeliculas.component";
+import { HomeComponent } from "./home/home.component";
+import { MovieComponent } from "./movie/movie.component";
+import { MovieDetailsComponent } from "./movie-details/movie-details.component";
+import { NowPlayingMoviesComponent } from "./now-playing-movies/now-playing-movies.component";
+import { PopularMoviesComponent } from "./popular-movies/popular-movies.component";
 
 @NgModule({
   declarations: [
@@ -20,11 +20,8 @@ import { PopularMoviesComponent } from './popular-movies/popular-movies.componen
     NowPlayingMoviesComponent,
     PopularMoviesComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
