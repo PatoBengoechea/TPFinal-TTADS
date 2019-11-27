b43c7c76644e11cb0fa03057dc5990378c7a7bd7
uoome <nicogomezwp@gmail.com>
2019-11-27 19:31:25 -0300

    Cambios menores de texto

--- frontend/src/app/movie/movie.component.ts
+++ frontend/src/app/movie/movie.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 // Routing
-import {ActivatedRoute, Router} from "@angular/router";
-
-
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-movie',
-  templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  selector: "app-movie",
+  templateUrl: "./movie.component.html",
+  styleUrls: ["./movie.component.css"]
 })
 export class MovieComponent implements OnInit {
-
   @Input() movie;
   private imgBaseURL = "https://image.tmdb.org/t/p/";
   private imgPosterSize = "w780";
 
-  constructor(private route: Router) { }
+  constructor(private route: Router) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
-  search(id: number){
-    this.route.navigate(['movie-details', id])
+  search(id: number) {
+    this.route.navigate(["movie-details", id]);
   }
-  
 }
