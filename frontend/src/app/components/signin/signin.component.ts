5f08d3c6e34784aa6f36d3affe949c9b34177852
Patricio Bengoechea <pato.bengoechea@gmail.com>
2020-02-05 23:40:07 -0300

    loader added

--- frontend/src/app/components/signin/signin.component.ts
+++ frontend/src/app/components/signin/signin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../../services/auth.service";
 import { Router } from "@angular/router";
 import { stringify } from 'querystring';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: "app-signin",
@@ -12,11 +13,12 @@ export class SigninComponent implements OnInit {
   user = {};
   errorMessage : string;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private spinner: NgxSpinnerService) {}
 
   ngOnInit() {}
 
   signIn() {
+    this.spinner.show()
     this.authService.signIn(this.user).subscribe(
       res => {
         console.log(res);
@@ -24,11 +26,13 @@ export class SigninComponent implements OnInit {
         localStorage.setItem("token", res.data.token);
         // Redireccionar a now playing movies
         this.router.navigate(["/now-playing"]);
+        this.spinner.hide()
       },
       err => {
         // console.log(err);
         this.errorMessage = err.error.message;
         // this.router.navigate(["/signin"]);
+        this.spinner.hide()
       }
     );
   }
