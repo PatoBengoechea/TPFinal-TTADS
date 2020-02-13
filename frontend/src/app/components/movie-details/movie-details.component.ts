12bbdc3368754a7a6ff03a36a963763df6434f1e
Patricio Bengoechea <pato.bengoechea@gmail.com>
2020-02-13 18:09:33 -0300

    vote component

--- frontend/src/app/components/movie-details/movie-details.component.ts
+++ frontend/src/app/components/movie-details/movie-details.component.ts
@@ -14,8 +14,8 @@ export class MovieDetailsComponent implements OnInit {
 
   @Input() movie: any = {};
   private monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+  "July", "August", "September", "October", "November", "December" ];
+  private vote = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   constructor(
     private service: ApiThemoviedbService,
